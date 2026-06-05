fee88347248ca1b3dbb7f743a5bcfa61c77dba5f
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-06-05 04:34:33 +0000

    [Finish] 3-2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "lists/show"
   get "lists/edit"
   get "/top" => "homes#top"
+
+  post 'lists' => "lists#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
