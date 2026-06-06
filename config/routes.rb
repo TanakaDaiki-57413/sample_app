f94ae68006754d9fc0fb72fe41aa03a16ec1573f
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-06-06 05:22:20 +0000

    [Finish] 3-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   get "lists/new"
-  get "lists/edit"
   get "/top" => "homes#top"
 
   get "lists" => "lists#index"
   # get "lists/:id" => "lists#show"
   # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list'
-  
+
+  # editアクションルーティング
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch "lists/:id" => "lists#update",as: "update_list"
+
   post 'lists' => "lists#create"
   
 
